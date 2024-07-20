9fcd04e8b95935e8b8fa0b9bf7f66665a5375553
Francisco Vanderson Oliveira Araújo <95548483+vandersonmgpb@users.noreply.github.com>
2024-07-20 06:53:21 -0300

    Integrando testes com Tesults

---
+++ runner.js
@@ -0,0 +1,17 @@
+const cypress = require('cypress')
+const tesults = require('cypress-tesults-reporter');
+
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImYyMjJiOGNiLWIyNGItNGEwNi05NWYyLWNmZjQ3MzU1MmM3OS0xNzIxNDY4NTM3MDg1IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiOGFjOWY4YWYtMjMyMi00NTVjLWE4YzktYTNjMDQ1MzYzNzBmIiwidHlwZSI6InQifQ.IM7RghimVeeJhPQ0fkiZujCGiGFO9lNpKNdC8A-boNU'
+
+cypress.run({
+  // specs to run here
+})
+.then((results) => {
+  const args = {
+    target: 'token',
+  }
+  tesults.results(results, args);
+})
+.catch((err) => {
+ console.error(err)
+})
